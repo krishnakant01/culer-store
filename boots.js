560be9dc76bcd6c4e92dcd47bc03f3467f255bd8
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-10-17 20:04:33 +0530

    products added to database, like button created.

--- boots.js
+++ boots.js
@@ -21,6 +21,18 @@ boots.map(boot => {
     img.src = boot.productImage;
     imageDiv.appendChild(img);
 
+    //creating wishlist button
+    var wButton = document.createElement("button");
+    var btnText = document.createElement("span");
+    btnText.classList.add("material-icons");
+    btnText.classList.add("wishlist");
+    btnText.innerText = "favorite_border";
+
+    wButton.classList.add("wishlist-btn");
+
+    wButton.appendChild(btnText);
+    imageDiv.appendChild(wButton);
+
     bootsCard.appendChild(imageDiv);
 
     //creating product name
@@ -46,12 +58,14 @@ boots.map(boot => {
     cardsContainer.appendChild(bootsCard);
 });
 
-// creating cart badge and updating its number, storing the items in local storage
 onReloadCartBadgeNumber();
+
+// creating cart badge and updating its number, storing the items in local storage
 var btnAddToCart = document.querySelectorAll(".product-add-to-cart");
+var btnWishlist = document.querySelectorAll(".wishlist-btn");
+var wishlistBtnText = document.querySelectorAll(".wishlist");
 
 for (let i = 0; i < btnAddToCart.length; i++) {
-
 
     btnAddToCart[i].addEventListener("click", () => {
 
@@ -65,12 +79,89 @@ for (let i = 0; i < btnAddToCart.length; i++) {
             storedCartNumber++;
             document.querySelector(".cart-badge").innerText = Number(storedCartNumber);
         }
-        
+
         cartBadgeNumber();
-        storeClickedProducts(boots[i]);
+        storeCartProducts(boots[i]);
         totalCartCost(boots[i]);
 
     });
+}
+
+for (let i = 0; i < btnWishlist.length; i++) {
+
+    btnWishlist[i].addEventListener("click", ()=>{
+     
+        storeWishlistProducts(boots[i], wishlistBtnText[i]);
+      //  setWishlistButtonState(boots[i]);
+
+    });
+}
+
+function storeWishlistProducts(product, text) {
+   
+    let wishlistItems = localStorage.getItem('productsInWishlist');
+    wishlistItems = JSON.parse(wishlistItems);
+
+    if (wishlistItems != null) {
+
+        if (wishlistItems[product.id] === undefined) {
+
+            wishlistItems = {
+                ...wishlistItems,
+                [product.id]: product
+            }
+            wishlistItems[product.id].liked = true;
+            text.innerText = "favorite";
+            text.style.color ="red";
+
+        } else {
+            wishlistItems[product.id].liked = false;
+            delete wishlistItems[product.id];
+            text.innerText = "favorite_border";
+            text.style.color ="black";
+
+            // if(Object.keys(wishlistItems).length == 0){
+            //     localStorage.removeItem("productsInWishlist");
+            // }
+        }
+    } else {
+        product.liked = true;
+        wishlistItems = {
+            [product.id]: product
+        }
+        text.innerText = "favorite";
+    }
+
+    localStorage.setItem('productsInWishlist', JSON.stringify(wishlistItems));
+  
+}
+//wishlist button state (liked, not liked)
+
+onReloadWishlistButtonState();
+function onReloadWishlistButtonState(){
+    var text = document.querySelectorAll(".wishlist");
+    let items = localStorage.getItem('productsInWishlist');
+    items = JSON.parse(items);
+    var result = [];
+    for( var i in items){
+        result.push(items[i]);
+    }
+    
+    for(let i=0; i<result.length; i++){
+       
+       // console.log(result[i].id);
+
+        if(items[result[i].id].liked == true){
+            text[result[i].id].innerText = "favorite";
+            text[result[i].id].style.color = "red";
+        }
+        else{
+            text[result[i].id].innerText = "favorite_border";
+            text[result[i].id].style.color = "black";
+        }
+    }
+
+
 }
 
 // create badge if there's no item added previously
@@ -109,10 +200,10 @@ function onReloadCartBadgeNumber() {
 }
 
 // store the selected product in local storage
-function storeClickedProducts(product) {
+function storeCartProducts(product) {
     let cartItems = localStorage.getItem('productsInCart');
     cartItems = JSON.parse(cartItems);
-    
+
     if (cartItems != null) {
 
         if (cartItems[product.id] === undefined) {
@@ -134,14 +225,14 @@ function storeClickedProducts(product) {
 }
 
 // Calculating Total Cost
-function totalCartCost(product){
+function totalCartCost(product) {
 
     let cartCost = localStorage.getItem('totalCartCost');
 
-    if(cartCost!=null){
+    if (cartCost != null) {
         localStorage.setItem("totalCartCost", Number(cartCost) + Number(product.productPrice));
-    
-    }else{
+
+    } else {
         localStorage.setItem("totalCartCost", product.productPrice);
     }
 }
--- database.js
+++ database.js
@@ -5,9 +5,9 @@ export const productCards = {
             id: 0,
             productImage: "./images/adidas-x-19.1-sg.png",
             productName: "Adidas X 19.1 SG",
-            productPrice: "3999",
+            productPrice: "4100",
             productTag: "boots",
-            addedInCart: false,
+            brand: "adidas",
             liked: false,
             inCart: 0
         },
@@ -16,9 +16,9 @@ export const productCards = {
             id: 1,
             productImage: "./images/adidas-predator-freak-fg.png",
             productName: "Adidas Predator FG",
-            productPrice: "5999",
+            productPrice: "6300",
             productTag: "boots",
-            addedInCart: false,
+            brand: "adidas",
             liked: false,
             inCart: 0
         },
@@ -27,9 +27,9 @@ export const productCards = {
             id: 2,
             productImage: "./images/nike-mercurial-vapor-academy-fg.png",
             productName: "Nike Mercurial Vapor FG",
-            productPrice: "4999",
+            productPrice: "4000",
             productTag: "kits",
-            addedInCart: false,
+            brand: "nike",
             liked: false,
             inCart: 0
         },
@@ -38,12 +38,101 @@ export const productCards = {
             id: 3,
             productImage: "./images/adidas-x-speedflow.png",
             productName: "Adidas X Speedflow+ FG",
-            productPrice: "8999",
+            productPrice: "9000",
             productTag: "kits",
-            addedInCart: false,
+            brand: "adidas",
             liked: false,
             inCart: 0
         },
+
+        {
+            id: 4,
+            productImage: "./images/adidas-x-speedflow-messi-sg.png",
+            productName: "Adidas X Speedflow Messi SG",
+            productPrice: "12000",
+            productTag: "boots",
+            brand: "adidas",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 5,
+            productImage: "./images/puma-future-5.3-netfit-fg.png",
+            productName: "Puma Future 5.3 Netfit FG",
+            productPrice: "3500",
+            productTag: "boots",
+            brand: "puma",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 6,
+            productImage: "./images/puma-one-5.1-sg.png",
+            productName: "Puma One 5.1 SG",
+            productPrice: "3000",
+            productTag: "boots",
+            brand: "puma",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 7,
+            productImage: "./images/nike-phantom-gt-II-elite-fg.png",
+            productName: "Nike Phantom GT II FG",
+            productPrice: "6000",
+            productTag: "boots",
+            brand: "nike",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 8,
+            productImage: "./images/nike-mercurial-superfly-8-elite-sg.png",
+            productName: "Nike Mercurial Superfly VIII SG",
+            productPrice: "7400",
+            productTag: "boots",
+            brand: "nike",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 9,
+            productImage: "./images/nike-phantom-gt-II-pro-fg.png",
+            productName: "Nike Phantom GT II Pro FG",
+            productPrice: "7000",
+            productTag: "boots",
+            brand: "nike",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 10,
+            productImage: "./images/puma-ultra-sl-tech-origin-gf.png",
+            productName: "Puma Ultra SL Origin FG",
+            productPrice: "6500",
+            productTag: "boots",
+            brand: "puma",
+            liked: false,
+            inCart: 0
+        },
+
+        {
+            id: 11,
+            productImage: "./images/puma-future-z-1.2-mg.png",
+            productName: "Puma Future Z 1.2 MG",
+            productPrice: "4000",
+            productTag: "boots",
+            brand: "puma",
+            liked: false,
+            inCart: 0
+        },
+        
 
     ]
 
